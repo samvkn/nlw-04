3fa29a1835b21552d7b13383165aa57dd913ecba
Samuel Clinton <contato@samvkn.dev>
2021-02-26 12:06:24 -0300

    Dia 2

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,10 +1,12 @@
-// Importando o micro-framework Express
-import express from 'express'
+import 'reflect-metadata';
+import express from 'express';
+import './database';
+import { router } from './routes';
 
-// Guardando o Express dentro de uma const app
 const app = express()
 
-// Fazendo o Express "escutar" a porta 3333 e exibindo uma mensagem no console 
-// é o equivalente a express().listen() já que antes guardamos o express dentro da const app
+app.use(express.json())
+
+app.use(router);
 
 app.listen(3333, () => console.log('Server is running'))
